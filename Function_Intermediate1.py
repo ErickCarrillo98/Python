aaf13d8875c6788b83269ab8f04fcea0611dfc29
ErickCarrillo98 <82337484+ErickCarrillo98@users.noreply.github.com>
2021-06-08 18:41:27 -0700

    Add files via upload

---
+++ Function_Intermediate1.py
@@ -0,0 +1,78 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+
+x[1][0] = 15
+print(x)
+
+students[0]['last_name'] = 'Bryant'
+print(students)
+
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory)
+
+z[0]['y'] = 30
+print(z)
+
+
+
+
+
+
+
+#Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+#Change the last_name of the first student from 'Jordan' to 'Bryant'
+#In the sports_directory, change 'Messi' to 'Andres'
+#Change the value 20 in z to 30
+
+#2 Iterate through a list of dictionaries
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary(some_list):
+        for i in some_list:
+            print(i)
+            
+iterateDictionary(students) 
+
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+#first_name - Michael, last_name - Jordan
+#first_name - John, last_name - Rosales
+#first_name - Mark, last_name - Guillen
+#first_name - KB, last_name - Tonel
+
+
+
+#3 Get Values from a list of dictionaries
+def iterate_dictionary2(some_list, key):
+    for i in some_list:
+        print(i[key])
+
+iterate_dictionary2(students, 'last_name')
+
+#4Iterate through a dictionary with list values
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def print_info(some_dict):
+    print((len(some_dict['locations'])), "locations", (some_dict['locations']))
+    print((len(some_dict['instructors'])), "instructors", (some_dict['instructors']))
+
+
+print_info(dojo)
